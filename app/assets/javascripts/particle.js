0877a9ac7257c3b5293220885fe23b9c7cedbcba
hori <hori@example.com>
2016-01-18 14:26:17 +0000

    first_branch

---
+++ app/assets/javascripts/particle.js
@@ -0,0 +1,7 @@
+
+$('#particles').particleground({ //要素の指定
+    dotColor:  'blue',　　　　//ドットの色
+    lineColor: 'blue',        //線の色
+    particleRadius: 7          //ドットのサイズ
+});
+$('#particles').css('background','white');
